542aaf5f895ce5465e89034719ee5b6c4f5433ee
m.ahmadifar <m.ahmadifar@aldyexpress.com>
2023-01-07 11:13:39 +0330

    race condation

--- routes/api.php
+++ routes/api.php
@@ -20,7 +20,7 @@ Route::post('get-token', [AuthController::class, 'getToken']);
 
 Route::middleware('drivers')->group(function () {
     Route::post('send-location', [DriverController::class, 'sendLocation']);
-    Route::post('orders/assign/{order}', [OrderController::class, 'assign']);
+    Route::post('orders/assign/{id}', [OrderController::class, 'assign']);
     Route::post('orders/delivered/{order}', [OrderController::class, 'delivered']);
     Route::get('orders/new-orders', [OrderController::class, 'newOrder']);
 });
